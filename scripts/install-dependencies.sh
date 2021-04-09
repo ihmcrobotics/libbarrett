75eafdba4c3c34a63e1122c6faf9bac7e03a931e
Brian Zenowich <bz@barrett.com>
2021-04-09 15:01:19 -0400

    Fixing more installation errors

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -12,11 +12,18 @@ if [ "$DISTRIB_RELEASE" = "20.04" ]; then
 	
 	# Install libbarrett dependencies
 	sudo apt update
-	sudo apt install -y linux-lowlatency-hwe-20.04
+	sudo apt install -y linux-lowlatency
 	sudo apt install -y git cmake clang net-tools can-utils
 	sudo apt install -y libgsl-dev libeigen3-dev libncurses-dev pkg-config 
 	sudo apt install -y libboost-system-dev libboost-thread-dev libboost-python-dev
-	# sudo apt-get install -y libboost-all-dev
+
+	# Pin the new kernel
+	sudo apt-mark hold linux-lowlatency
+	
+	# Archive the other kernels
+	sudo mkdir /boot/archive
+	sudo mv /boot/*generic /boot/archive
+	sudo update-grub
 
 	#Download and Install patched Libconfig 1.4.5 (supporting C & C++ simultaneously)
 	cd ~/Downloads && wget http://web.barrett.com/support/WAM_Installer/libconfig-1.4.5-PATCHED.tar.gz
